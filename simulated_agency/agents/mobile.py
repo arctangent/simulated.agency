d70b221ae16974d6ea0a8b21e5e5d7acf171ae77
Jacob Conrad Martin <arctangent@gmail.com>
2018-06-10 11:21:05 +0100

    Alternative movement strategies when blocked

--- simulated_agency/agents/mobile.py
+++ simulated_agency/agents/mobile.py
@@ -23,22 +23,74 @@ class Mobile(Locatable):
     def __repr__(self):
         return 'Mobile with state %s at (%s, %s)' % (self._state_stack.peek(), self.location.x, self.location.y)
 
-    def move_to_location(self, new_location):
+    def _relocate(self, new_location):
+        '''
+        Internal method to perform a known-successfuk relocation.
+        Use move_to_location instead of calling this directly.
+        '''
+        # Remove from current location
+        self.location.contents.remove(self)
+        # Add to new location
+        self.location = new_location
+        new_location.contents.append(self)
+
+    def move_to_location(self, new_location, alternatives=[]):
         '''
         Move the agent to a specified location
         '''
         
-        # Check that proposed new location can accommodate us
+        # If we cannot move to the proposed new location
+        # then we try to find a sensible alternative
         if not new_location.can_fit(self):
-            # Do nothing
-            return
-        
-        # Remove from current location
-        self.location.contents.remove(self)
+            # Were any alternative moves supplied?
+            if alternatives:
+                viable_alternatives = [alt for alt in alternatives if alt.can_fit(self)]
+                if viable_alternatives:
+                    new_location = choice(viable_alternatives)
+                    self._relocate(new_location)
+                else:
+                    # None of the specified alternatives
+                    # was available, so do nothing
+                    pass
 
-        # Add to new location
-        self.location = new_location
-        new_location.contents.append(self)
+            # No alternatives specified, so we will do the best we can.
+            # Identify which axis we were trying to move on
+            if self.location.x - new_location.x == 0:
+                # Can't move up/down, but maybe left or right would work?
+                left = new_location.left()
+                right = new_location.right()
+                left_available = left.can_fit(self)
+                right_available = right.can_fit(self)
+                # Determine alternative location
+                if left_available and right_available:
+                    new_location = choice([self.location.left(), self.location.right()])
+                elif left_available:
+                    new_location = self.location.left()
+                elif right_available:
+                    new_location = self.location.right()
+                else:
+                    # We failed to find an alternative, so do nothing
+                    return
+            else:
+                # Attempted an left/right move
+                # Can't move left/right, but maybe up or down would work?
+                up = new_location.up()
+                down = new_location.down()
+                up_available = up.can_fit(self)
+                down_available = down.can_fit(self)
+                # Determine alternative location
+                if up_available and down_available:
+                    new_location = choice([self.location.up(), self.location.down()])
+                elif up_available:
+                    new_location = self.location.up()
+                elif down_available:
+                    new_location = self.location.down()
+                else:
+                    # We failed to find an alternative, so do nothing
+                    return 
+        
+        # Perform the relocation
+        self._relocate(new_location)
 
     def move_randomly(self):
         location = choice(self.location.neighbourhood())
@@ -87,6 +139,11 @@ class Mobile(Locatable):
         height = simulation.height
         half_width = width / 2
         half_height = height / 2
+        move = self.move_to_location
+        up = self.location.up()
+        down = self.location.down()
+        left = self.location.left()
+        right = self.location.right()
 
         # Compute naive, non-wrapping distance
         dx = target_x - self.location.x
@@ -126,21 +183,22 @@ class Mobile(Locatable):
 
         # Decide which direction to move in depending
         # on the magnitudes of the component parts of the vector
+
         if dx == dy == 0:
             # We're at the location
             pass
         elif dx == 0:
             # Move in direction of dy
             if dy > 0:
-                self.move_to_location(self.location.down())
+                move(down)
             else:
-                self.move_to_location(self.location.up())
+                move(up)
         elif dy == 0:
             # Move in direction of dx
             if dx > 0:
-                self.move_to_location(self.location.right())
+                move(right)
             else:
-                self.move_to_location(self.location.left())
+                move(left)
         else:
             # Decide stochastically which direction to move in
             abs_dx = abs(dx)
@@ -151,12 +209,32 @@ class Mobile(Locatable):
             if dice_roll <= abs_dx:
                 # Move in direction of dx
                 if dx > 0:
-                    self.move_to_location(self.location.right())
+                    if dy > 0:
+                        move(right, alternatives=[down])
+                    elif dy < 0:
+                        move(right, alternatives=[up])
+                    else:
+                        move(right)
                 else:
-                    self.move_to_location(self.location.left())
+                    if dy > 0:
+                        move(left, alternatives=[down])
+                    elif dy < 0:
+                        move(left, alternatives=[up])
+                    else:
+                        move(left)
             else:
                 # Move in direction of dy
                 if dy > 0:
-                    self.move_to_location(self.location.down())
+                    if dx > 0:
+                        move(down, alternatives=[right])
+                    elif dx < 0:
+                        move(down, alternatives=[left])
+                    else:
+                        move(down)
                 else:
-                    self.move_to_location(self.location.up())
+                    if dx > 0:
+                        move(up, alternatives=[right])
+                    elif dx < 0:
+                        move(up, alternatives=[left])
+                    else:
+                        move(up)
