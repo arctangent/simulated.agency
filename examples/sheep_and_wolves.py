d13ea51bca502c775bb3ddb31ab27c98c2b646d5
Jacob Conrad Martin <arctangent@gmail.com>
2018-07-15 13:49:30 +0100

    Redoing models for sheep and wolves

--- examples/sheep_and_wolves.py
+++ examples/sheep_and_wolves.py
@@ -1,75 +1,118 @@
 
-from random import choice
+from random import choice, randint
 
 from simulated_agency.simulation import Simulation
 from simulated_agency.agents import Mobile
 from simulated_agency.states import *
 
 
-class KillTarget(State):
+class WolfChasingTarget(State):
     '''
-    Agents in this state will kill a target
-    if it is adjacent to them, otherwise they will
-    do nothing and proceed to the next state.
+    A wolf in this state will move towards its
+    target and kill that target if it ends up
+    adjacent to the target after the move.
     '''
 
-    name = 'DESTROY_TARGET'
-    colour = 'red'
+    name = "WOLF_CHASING_TARGET"
+    colour = 'blue'
     required_params = ['target']
 
     def handle(self):
+        super().handle()
         target = self.context['target']
+        agent = self.agent
+        agent.move_towards_target(target)
         if target in self.agent.location.neighbours():
             target.replace_state(Dead)
-        self.agent.remove_state()
+            agent.replace_state(WolfSelectingTarget)
 
 
-class ChooseTargetToFollow(State):
+class WolfSelectingTarget(State):
     '''
     Agents in this state will choose a target
     to follow and then begin following it.
-
-    Note that due to the design of the MoveTowardsTarget
-    state, they will return to this state after getting
-    close to their target and thus choose another one.
     '''
 
-    name = 'CHOOSE_TARGET_TO_FOLLOW'
-    colour = 'yellow'
-    required_params = ['prey']
+    name = 'WOLF_SELECTING_TARGET'
+    colour = 'blue'
 
     def handle(self):
-        target_list = self.context['prey'].objects
+        super().handle()
+        target_list = Sheep.objects
         # Wolves will only chase Sheep that are alive
         live_target_list = [x for x in target_list if not x.is_in_state(Dead)]
         # Choose target
         if live_target_list:
-            # Choose the nearest live target
+            # Choose the nearest live target and pursue it
             target = self.agent.nearest(live_target_list)
-            # Assign the state
-            # When we have done moving towards the target we will kill it
-            self.agent.add_state(KillTarget, target=target)
-            # Remember, the last state added is the first to execute
-            self.agent.add_state(MoveTowardsTarget, target=target)
+            self.agent.add_state(WolfChasingTarget, target=target)
         else:
             # No sheep left to chase
             self.agent.add_state(MoveRandomly)
 
+
+class SheepGrazing(State):
+    '''
+    Sheep that are grazing occasionally move around.
+    But mainly they are recovering their energy.
+    '''
+    
+    name =  'SHEEP_GRAZING'
+    colour = 'green'
+
+    def handle(self):
+        super().handle()
+        agent = self.agent
+        # Restore energy up to max
+        if agent.energy < 5:
+            agent.energy += 1
+        # Is there a wolf nearby?
+        nearest_wolf = agent.nearest(Wolf.objects)
+        if agent.distance_to(nearest_wolf) <= 3:
+            agent.replace_state(SheepFleeing, enemy=nearest_wolf)
+            return
+        # Occasionally move
+        if randint(1, 20) == 1:
+            self.agent.move_randomly()
+
+
+class SheepFleeing(State):
+    '''
+    Sheep that are fleeing will do so until they tire.
+    '''
+
+    name = 'SHEEP_FLEEING'
+    colour = 'green'
+    required_params = ['enemy']
+
+    def handle(self):
+        super().handle()
+        agent = self.agent
+        # Burn energy
+        agent.energy -= 1
+        if agent.energy == 0:
+            agent.replace_state(SheepGrazing)
+            return
+        # Flee
+        enemy = self.context['enemy']
+        agent.move_away_from_target(enemy)
+
+
 # Initialise simulation
-simulation = Simulation(cell_size=8, name='SheepAndWolves')
+simulation = Simulation(cell_size=16, name='SheepAndWolves')
 
 # Use same base model for two types of object
-class Sheep(Mobile): pass
-class Wolves(Mobile): pass
+class Sheep(Mobile): energy = 5
+class Wolf(Mobile): pass
 
 # Bind models to simulation
-simulation.bind(Wolves, Sheep)
+simulation.bind(Wolf, Sheep)
 
 # Add some sheep to the simulation
-simulation.seed(Sheep, 0.15, AvoidType, enemy=Wolves, comfort_zone=3)
+simulation.seed(Sheep, 0.15, SheepGrazing)
 
 # Add some wolves to the simulation
-simulation.seed(Wolves, 50, ChooseTargetToFollow, prey=Sheep)
+simulation.seed(Wolf, 15, WolfSelectingTarget)
 
 # Run the simulation
 simulation.execute(draw_locations=False)
